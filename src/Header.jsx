7221e193049cb6d958b874cd836b84ef85432bdd
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-08-26 20:32:44 -0400

    moved

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 const Header = ({ setCurrentPage }) => {
   return (
     <div className='fixed top-0 right-0 left-0 z-50 flex w-full items-center justify-between gap-4 bg-gray-900 bg-opacity-80 py-4 pr-4 md:pr-8 lg:pr-16 backdrop-blur-md'>
-      <a href='/Flipper' className='flex' title='Dashboard'>
+      <a href='/Flipper/' className='flex' title='Dashboard'>
         <img src='./src/images/flipper.png' width='64' />
       </a>
       <div className='flex flex-row items-center gap-4 md:gap-8 h-8'>
@@ -10,9 +10,6 @@ const Header = ({ setCurrentPage }) => {
         </button>
         <button title='search' onClick={() => setCurrentPage('search')}>
           Search
-        </button>
-        <button title='account' onClick={() => setCurrentPage('account')}>
-          Account
         </button>
       </div>
     </div>
